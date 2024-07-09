9a53dea055f8a548d4cf1438784d7c544e749a7a
mehjbeen22 <mehjbeen22@navgurukul.org>
2024-07-09 17:23:35 +0530

    +1

---
+++ src/pages/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="bg-black text-gray-100 p-4 text-center">
+      <p className="mt-4">
+        Copyright © 2024 PrintLiveSol. All Rights Reserved.
+      </p>
+    </footer>
+  );
+}
